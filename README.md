1d9d3c42b7bf3d15ba0042edd04f180646dfd51a
lafeillou <lafeillou@126.com>
2023-03-10 15:03:29 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vue-demo-nextTick
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vue-7fuozz)
---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <NextTick />
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue';
+import NextTick from './components/NextTick.vue';
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    NextTick,
+  },
+};
+</script>
+
+<style>
+#app {
+  /* font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px; */
+}
+</style>
